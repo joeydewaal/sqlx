014e12255f82d4cf4bc189c752f48c78205ad8c7
Joey de Waal <dewaaljoey@gmail.com>
2025-06-07 14:27:54 +0200

    sqlx-postgres: Remove PgStream from PgConnection

--- sqlx-postgres/src/connection/mod.rs
+++ sqlx-postgres/src/connection/mod.rs
@@ -23,8 +23,6 @@ use crate::{PgConnectOptions, PgTypeInfo, Postgres};
 
 pub(crate) use sqlx_core::connection::*;
 
-pub use self::stream::PgStream;
-
 pub(crate) mod describe;
 mod establish;
 mod executor;
@@ -43,11 +41,6 @@ pub struct PgConnection {
 }
 
 pub struct PgConnectionInner {
-    // underlying TCP or UDS stream,
-    // wrapped in a potentially TLS stream,
-    // wrapped in a buffered stream
-    pub(crate) stream: PgStream,
-
     chan: UnboundedSender<IoRequest>,
 
     pub(crate) notifications: UnboundedReceiver<Notification>,
@@ -112,7 +105,6 @@ impl PgConnection {
     }
 
     fn new(
-        stream: PgStream,
         options: &PgConnectOptions,
         chan: UnboundedSender<IoRequest>,
         notifications: UnboundedReceiver<Notification>,
@@ -132,7 +124,6 @@ impl PgConnection {
                 cache_elem_type_to_array: HashMap::new(),
                 cache_table_to_column_names: HashMap::new(),
                 transaction_depth: 0,
-                stream,
                 server_version_num: None,
                 shared,
             }),
@@ -289,7 +280,7 @@ impl Connection for PgConnection {
     }
 
     fn shrink_buffers(&mut self) {
-        self.inner.stream.shrink_buffers();
+        // No-op
     }
 
     #[doc(hidden)]
@@ -299,7 +290,7 @@ impl Connection for PgConnection {
 
     #[doc(hidden)]
     fn should_flush(&self) -> bool {
-        !self.inner.stream.write_buffer().is_empty()
+        false
     }
 }
 
